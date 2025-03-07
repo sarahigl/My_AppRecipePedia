7c601237ccd8bb44137692230d7618d03f4fb475
sarah <sarah.iglla@gmail.com>
2025-03-07 16:11:10 +0100

    Sécurisation inputs : user & IA bot avec regex + ajout max char et factorisation de la fonction sendMessage() => appel API se fait dans la fonction makeApiCall() + gestion des erreurs coté user

--- app/src/main/java/com/example/myapplication/Views/IA/ChatIAFragment.java
+++ app/src/main/java/com/example/myapplication/Views/IA/ChatIAFragment.java
@@ -15,6 +15,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.myapplication.Model.IA.ChatMessage;
 import com.example.myapplication.Model.IA.DTO.Message;
@@ -52,66 +53,97 @@ public class ChatIAFragment extends Fragment {
         binding = FragmentChatIABinding.inflate(inflater, container, false);
         inputchat = binding.inputchat;
         requeteIAViewModel = new ViewModelProvider(requireActivity()).get(RequeteIAViewModel.class);
-
         //Initialisation de RecyclerView et du gestionnaire de disposition// Obtient la référence au RecyclerView à partir du layout lié
         recyclerView= binding.rvchatUser;
         LinearLayoutManager linearLayoutManagerUser = new LinearLayoutManager(getActivity());//getActivity est l'équivalent de class.this
         recyclerView.setLayoutManager(linearLayoutManagerUser);
-
         //api service
         apiServiceIA = RetrofitClient.getApiServiceIA();
-
-        // Initialize the adapter for user msg
+        //initilisation de l'adapter
         chatMessageAdapter= new AdapterChatMessage(chatMessages);
         recyclerView.setAdapter(chatMessageAdapter);
-
         //send function
         binding.sendmessage.setOnClickListener(this::sendMessage);
-
         return binding.getRoot();
     }
 
+    public void makeApiCall(OpenAiRequete openAiRequete){
+        //API call
+        Log.d("ChatIAFragment", "Making API call...");
+        apiServiceIA.createRequest(openAiRequete).enqueue(new Callback<OpenAiReponse>() {
+            @SuppressLint("RestrictedApi")
+            @Override
+            public void onResponse(Call<OpenAiReponse> call, Response<OpenAiReponse> response) {
+                Log.d("ChatIAFragment", "API call onResponse");
+                if(response.isSuccessful() && response.body() != null){
+                    OpenAiReponse reponse = response.body();
+                    String botMessage = reponse.getMessage();
+                    // Sanitize le message IA pour ne garder que les caractères alphanumériques
+                    botMessage = botMessage.replaceAll("[^a-zA-Z0-9\\s.,?!'()àâäéèêëîïôöùûüçœŒ-]", "");
+                    botMessage = botMessage.trim();
+                    //Log.d("D/ChatIAFragment", "ReponseIA : " + reponse.getMessage());
+                    chatMessageAdapter.addMessage(new ChatMessage(botMessage, ChatMessage.TYPE_BOT));
+                } else {
+                    Log.e("ChatIAFragment", "API call failed with code: " + response.code());
+                    switch (response.code()) {
+                        case 401 :
+                            Toast.makeText(getActivity(), "Erreur d'authentification. Veuillez vérifier que vous avez bien renseigné votre clé API", Toast.LENGTH_SHORT).show();
+                            break;
+                        case 403:
+                            Toast.makeText(getContext(), "Vous avez peut-être dépassé la limite de requêtes.", Toast.LENGTH_LONG).show();
+                            break;
+                        case 500:
+                            Toast.makeText(getContext(), "Erreur interne du serveur. Veuillez réessayer plus tard.", Toast.LENGTH_LONG).show();
+                            break;
+                        default:
+                            Toast.makeText(getContext(), "Une erreur est survenue. Veuillez réessayer plus tard.", Toast.LENGTH_LONG).show();
+                            break;
+                    }
+                }
+            }
+            @Override
+            public void onFailure(Call<OpenAiReponse> call, Throwable t) {
+                Log.e("E/ChatIAFragment", "API call failed", t);
+                Toast.makeText(getContext(), "Erreur de connexion. Veuillez vérifier votre connexion Internet ou réessayer plus tard.", Toast.LENGTH_LONG).show();
+                binding.inputchat.setText("");
+                //affichage d'un message d'erreur dans la discussion
+                chatMessageAdapter.addMessage(new ChatMessage("Erreur de connexion", ChatMessage.TYPE_BOT));
+            }
+        });
+
+    }
     public void sendMessage(View view) {
         String date = DateFormat.getDateTimeInstance().format(Calendar.getInstance().getTime());
         String message = Objects.requireNonNull(binding.inputchat.getText()).toString().trim();
         Boolean status = false;
         //teporary solution to modifify later with id loggged user
-        int idUtilisateur = getLoggedInUserId();
-
-
-        if (message.length() >= 2 && !TextUtils.isEmpty(date)) {
-            chatMessageAdapter.addMessage(new ChatMessage(message, ChatMessage.TYPE_USER));
-            binding.inputchat.setText("");
-
-            //creation objet requeteOpenIA qui sera envoyé à openAI api
-            List<Message> messages = new ArrayList<>();
-            messages.add(new Message("system", "You are an expert in cooking."));
-            messages.add(new Message("user", message));
-            OpenAiRequete openAiRequete = new OpenAiRequete("gpt-4o-mini", messages);
-
-            //API call
-            Log.d("ChatIAFragment", "Making API call...");
-            apiServiceIA.createRequest(openAiRequete).enqueue(new Callback<OpenAiReponse>() {
-                @SuppressLint("RestrictedApi")
-                @Override
-                public void onResponse(Call<OpenAiReponse> call, Response<OpenAiReponse> response) {
-                    Log.d("ChatIAFragment", "API call onResponse");
-                    if(response.isSuccessful() && response.body() != null){
-                        OpenAiReponse reponse = response.body();
-                        Log.d("D/ChatIAFragment", "ReponseIA : " + reponse.getMessage());
-                        chatMessageAdapter.addMessage(new ChatMessage(reponse.getMessage(), ChatMessage.TYPE_BOT));
-                    } else {
-                        Log.e("ChatIAFragment", "API call failed with code: " + response.code());
-                    }
-                }
-                @Override
-                public void onFailure(Call<OpenAiReponse> call, Throwable t) {
-                    Log.e("E/ChatIAFragment", "API call failed", t);
-                }
-            });
-        }else{
-            binding.inputchat.setError("Veuillez saisir un message");
+        //int idUtilisateur = getLoggedInUserId();
+        //empeche les messages vides
+        if (message.length() < 2) {
+            binding.inputchat.setError("Veuillez saisir un message de plus de 2 caractères");
+            return;
         }
+        //empeche la surchage
+        if(message.length()>500){
+            binding.inputchat.setError("Message trop long (max 500 caractères)");
+            return;
+        }
+        // regex pour ne garder que les caractères autorisés
+        String regex = "^[a-zA-Z0-9\\s.,?!'()àâäéèêëîïôöùûüçœŒ-]+$";
+        if (!message.matches(regex)) {
+            message = message.replaceAll("[^a-zA-Z0-9\\s.,?!'()àâäéèêëîïôöùûüçœŒ-]", "");
+        }
+        chatMessageAdapter.addMessage(new ChatMessage(message, ChatMessage.TYPE_USER));
+        binding.inputchat.setText("");
+
+        //creation objet requeteOpenIA qui sera envoyé à openAI api
+        List<Message> messages = new ArrayList<>();
+        messages.add(new Message("system", "You are an expert in cooking."));
+        messages.add(new Message("user", message));
+        OpenAiRequete openAiRequete = new OpenAiRequete("gpt-4o-mini", messages);
+
+        //envoyer la requete à l'API
+        makeApiCall(openAiRequete);
     }
     private int getLoggedInUserId() {
         return 1; // Temporaire
